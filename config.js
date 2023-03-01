ff27380159cd19c05fa1c18809b3e6715a7c178d
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2023-03-02 00:09:34 +0100

    Refactor

--- config.js
+++ config.js
@@ -6,213 +6,221 @@
 // └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
 
 const CONFIG = {
-  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
-  // ├┴┐├─┤└─┐││  └─┐
-  // └─┘┴ ┴└─┘┴└─┘└─┘
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
 
-  // General
-  name: 'Yuriy',
-  imageBackground: true,
-  openInNewTab: false,
-  twelveHourFormat: false,
+	// General
+	name: "Yuriy",
+	openInNewTab: false,
+	twelveHourFormat: false,
+	title: "Dashboard",
 
-  // Greetings
-  greetingMorning: 'Good morning!',
-  greetingAfternoon: 'Good afternoon,',
-  greetingEvening: 'Good evening,',
-  greetingNight: 'Go to Sleep!',
+	// Theme
+	theme: "latte-mocha",
+	imageBackground: true,
+	imageUrl: "./assets/background.jpg", // Set custom background image URL. If the page is served insecurely, you may have issues loading images from pages over https.
 
-  // Layout
-  bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	// Greetings
+	greetingMorning: "Good morning!",
+	greetingAfternoon: "Good afternoon,",
+	greetingEvening: "Good evening,",
+	greetingNight: "Go to Sleep!",
 
-  // Weather
-  weatherKey: '005dfa6f655e32f1ceca39115347bab6', // Write here your API Key
-  weatherIcons: 'White', // 'Onedark', 'Nord', 'Dark', 'White'
-  weatherUnit: 'C', // 'F', 'C'
-  language: 'en', // More languages in https://openweathermap.org/current#multi
+	// Layout
+	bentoLayout: "bento", // 'bento', 'lists', 'buttons'
 
-  trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '43.723',
-  defaultLongitude: '10.402',
+	// Weather
+	weatherKey: "005dfa6f655e32f1ceca39115347bab6", // Write here your API Key
+	weatherIcons: "White", // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: "C", // 'F', 'C'
+	language: "en", // More languages in https://openweathermap.org/current#multi
 
-  // Autochange
-  autoChangeTheme: false,
+	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: "43.723",
+	defaultLongitude: "10.402",
 
-  // Autochabge by OS
-  changeThemeByOS: false,
+	// Autochange
+	autoChangeTheme: false,
 
-  // Autochange by hour options (24hrs format, string must be in: hh:mm)
-  changeThemeByHour: false,
-  hourDarkThemeActive: '18:30',
-  hourDarkThemeInactive: '07:00',
+	// Autochange by OS
+	changeThemeByOS: false,
 
-  // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
-  // ├┴┐│ │ │  │ │ ││││└─┐
-  // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: "18:30",
+	hourDarkThemeInactive: "07:00",
 
-  firstButtonsContainer: [
-    {
-      id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/',
-    },
-    {
-      id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'https://mail.google.com/',
-    },
-    {
-      id: '3',
-      name: 'Notion',
-      icon: 'album',
-      link: 'https://www.notion.so/dropbit/Main-Board-481c96ee740945d4b6c291f89f8b5d67',
-    },
-    {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
-    },
-    {
-      id: '5',
-      name: 'UniPi Agenda',
-      icon: 'graduation-cap',
-      link: 'https://agendadidattica.unipi.it/Prod/Home/Calendar',
-    },
-    {
-      id: '6',
-      name: 'Amazon',
-      icon: 'shopping-bag',
-      link: 'https://amazon.it/',
-    },
-  ],
+	// Autochange automatically based on location (sunrise/sunset). Openweathermap API key required.
+	changeThemeByLocation: false,
 
-  secondButtonsContainer: [
-    {
-      id: '1',
-      name: 'Music',
-      icon: 'headphones',
-      link: 'https://open.spotify.com',
-    },
-    {
-      id: '2',
-      name: 'twitter',
-      icon: 'twitter',
-      link: 'https://twitter.com/',
-    },
-    {
-      id: '3',
-      name: 'bot',
-      icon: 'bot',
-      link: 'https://discord.com/app',
-    },
-    {
-      id: '4',
-      name: 'Amazon',
-      icon: 'shopping-bag',
-      link: 'https://amazon.com/',
-    },
-  ],
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
 
-  // ┬  ┬┌─┐┌┬┐┌─┐
-  // │  │└─┐ │ └─┐
-  // ┴─┘┴└─┘ ┴ └─┘
+	firstButtonsContainer: [
+		{
+			id: "1",
+			name: "Github",
+			icon: "github",
+			link: "https://github.com/",
+		},
+		{
+			id: "2",
+			name: "Mail",
+			icon: "mail",
+			link: "https://mail.google.com/",
+		},
+		{
+			id: "3",
+			name: "Notion",
+			icon: "album",
+			link: "https://www.notion.so/dropbit/Main-Board-481c96ee740945d4b6c291f89f8b5d67",
+		},
+		{
+			id: "4",
+			name: "Calendar",
+			icon: "calendar",
+			link: "https://calendar.google.com/calendar/r",
+		},
+		{
+			id: "5",
+			name: "UniPi Agenda",
+			icon: "graduation-cap",
+			link: "https://agendadidattica.unipi.it/Prod/Home/Calendar",
+		},
+		{
+			id: "6",
+			name: "Amazon",
+			icon: "shopping-bag",
+			link: "https://amazon.it/",
+		},
+	],
 
-  // First Links Container
-  firstlistsContainer: [
-    {
-      icon: 'library',
-      id: '1',
-      links: [
-        {
-          name: 'Rust Book',
-          link: 'https://doc.rust-lang.org/book/',
-        },
-        {
-          name: 'Java Doc',
-          link: 'https://docs.oracle.com/javase/8/docs/api/',
-        },
-        {
-          name: 'Reddit',
-          link: 'https://www.reddit.com/',
-        },
-        {
-          name: 'Moodle',
-          link: 'https://elearning.di.unipi.it/',
-        },
-      ],
-    },
-    {
-      icon: 'coffee',
-      id: '2',
-      links: [
-        {
-          name: 'AnimeWorld',
-          link: 'https://www.animeworld.tv/',
-        },
-        {
-          name: 'YouTube',
-          link: 'https://www.youtube.com/',
-        },
-        {
-          name: 'Netflix',
-          link: 'https://www.netflix.com/browse',
-        },
-        {
-          name: 'Prime Video',
-          link: 'https://www.primevideo.com/',
-        },
-      ],
-    },
-  ],
+	secondButtonsContainer: [
+		{
+			id: "1",
+			name: "Music",
+			icon: "headphones",
+			link: "https://open.spotify.com",
+		},
+		{
+			id: "2",
+			name: "twitter",
+			icon: "twitter",
+			link: "https://twitter.com/",
+		},
+		{
+			id: "3",
+			name: "bot",
+			icon: "bot",
+			link: "https://discord.com/app",
+		},
+		{
+			id: "4",
+			name: "Amazon",
+			icon: "shopping-bag",
+			link: "https://amazon.com/",
+		},
+	],
 
-  // Second Links Container
-  secondListsContainer: [
-    {
-      icon: 'binary',
-      id: '1',
-      links: [
-        {
-          name: 'Spotify',
-          link: 'https://www.spotify.com',
-        },
-        {
-          name: 'Reddit',
-          link: 'https://www.reddit.com',
-        },
-        {
-          name: 'Hashnode',
-          link: 'https://www.hashnode.com',
-        },
-        {
-          name: 'Pocket',
-          link: 'https://www.pocket.com',
-        },
-      ],
-    },
-    {
-      icon: 'github',
-      id: '2',
-      links: [
-        {
-          name: 'Front',
-          link: 'https://www.reddit.com/r/Frontend/',
-        },
-        {
-          name: 'Rust',
-          link: 'https://www.reddit.com/r/rust/',
-        },
-        {
-          name: 'Go',
-          link: 'https://www.reddit.com/r/golang/',
-        },
-        {
-          name: 'Repos',
-          link: 'https://github.com/migueravila',
-        },
-      ],
-    },
-  ],
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: "library",
+			id: "1",
+			links: [
+				{
+					name: "Rust Book",
+					link: "https://doc.rust-lang.org/book/",
+				},
+				{
+					name: "Java Doc",
+					link: "https://docs.oracle.com/javase/8/docs/api/",
+				},
+				{
+					name: "Reddit",
+					link: "https://www.reddit.com/",
+				},
+				{
+					name: "Moodle",
+					link: "https://elearning.di.unipi.it/",
+				},
+			],
+		},
+		{
+			icon: "coffee",
+			id: "2",
+			links: [
+				{
+					name: "AnimeWorld",
+					link: "https://www.animeworld.tv/",
+				},
+				{
+					name: "YouTube",
+					link: "https://www.youtube.com/",
+				},
+				{
+					name: "Netflix",
+					link: "https://www.netflix.com/browse",
+				},
+				{
+					name: "Prime Video",
+					link: "https://www.primevideo.com/",
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: "binary",
+			id: "1",
+			links: [
+				{
+					name: "Spotify",
+					link: "https://www.spotify.com",
+				},
+				{
+					name: "Reddit",
+					link: "https://www.reddit.com",
+				},
+				{
+					name: "Hashnode",
+					link: "https://www.hashnode.com",
+				},
+				{
+					name: "Pocket",
+					link: "https://www.pocket.com",
+				},
+			],
+		},
+		{
+			icon: "github",
+			id: "2",
+			links: [
+				{
+					name: "Front",
+					link: "https://www.reddit.com/r/Frontend/",
+				},
+				{
+					name: "Rust",
+					link: "https://www.reddit.com/r/rust/",
+				},
+				{
+					name: "Go",
+					link: "https://www.reddit.com/r/golang/",
+				},
+				{
+					name: "Repos",
+					link: "https://github.com/migueravila",
+				},
+			],
+		},
+	],
 };
